beca6e9c1046896ef14663bd9c292ab841ebfe61
dylandasilva1999 <55394640+dylandasilva1999@users.noreply.github.com>
2020-10-11 20:13:46 +0200

    Add Headers Components
    -Added multiple header components html files and ng-include them for the home and products page.

--- app.js
+++ app.js
@@ -4,23 +4,32 @@ app.config(function ($routeProvider) {
 
     // Home Page Route
     $routeProvider.when("/home", {
-        templateUrl: "/index.html"
+        title: "Home",
+        controller: "HomeController",
+        templateUrl: "templates/home.html"
     });
 
     // Shoes Route (Products)
     $routeProvider.when("/shoes", {
-        templateUrl: "/templates/products.html"
+        title: "Shoes",
+        controller: "ProductsController",
+        templateUrl: "templates/products.html"
     });
 
     // Contact Route
     $routeProvider.when("/contact", {
+        title: "Contact",
         controller: "ContactController",
-        templateUrl: "/templates/contact.html"
+        templateUrl: "templates/contact.html"
     });
 });
 
-app.controller("IntroductionController", function ($scope) {
-    $scope.message = "Welcome to Connoisseur!"
+app.controller("HomeController", function ($scope) {
+    $scope.headerHome = "./templates/components/header-home.html";
+});
+
+app.controller("ProductsController", function ($scope) {
+    $scope.headerProducts = "./templates/components/header-products.html";
 });
 
 app.controller("ContactController", function ($scope) {
